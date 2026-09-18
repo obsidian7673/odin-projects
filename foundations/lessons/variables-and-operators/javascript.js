19f842e7365509e197423d01ce354c5fd72a3962
Rosemary <dev7673@proton.me>
2026-09-18 17:25:54 -0500

    Add variables and operators lesson

---
+++ foundations/lessons/variables-and-operators/javascript.js
@@ -0,0 +1,44 @@
+console.log("Hello from an external js file");
+
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(firstName + " " + lastName);
+
+let age = 11;
+console.log(age);
+
+age = 12;
+console.log(age);
+
+const pi = 3.14;
+console.log(pi);
+
+//pi = 10;
+//console.log(pi);
+
+console.log((3 + 2) - 76 * (1 + 1));
+
+console.log(23 + 97);
+
+console.log(2 + 4 + 8 + 16 + 32 + 64);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+a = a + a;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
+
+let add1 = prompt("First number?", 1);
+let add2 = prompt("Second number?", 2);
+console.log(add1 + add2);
